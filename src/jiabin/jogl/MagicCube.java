466b10f99add06254ee24dd930cfb08216f0d17b
xihadage <276423525@qq.com>
2018-11-05 21:03:02 +0800

    增加复原演示

--- src/jiabin/jogl/MagicCube.java
+++ src/jiabin/jogl/MagicCube.java
@@ -2,12 +2,19 @@ package jiabin.jogl;
 
 public class MagicCube {
 
-    public static final char[][] front = {{'y','o','o'},{'r','r','o'},{'b','b','w'}};
+/*    public static final char[][] front = {{'y','o','o'},{'r','r','o'},{'b','b','w'}};
     public static final char[][] back = {{'r','w','w'},{'w','o','b'},{'g','o','g'}};
     public static final char[][] left = {{'y','g','o'},{'w','b','g'},{'r','y','b'}};
     public static final char[][] right = {{'w','r','o'},{'r','g','y'},{'w','w','b'}};
     public static final char[][] up = {{'y','b','r'},{'o','y','g'},{'o','y','b'}};
-    public static final char[][] down = {{'g','b','g'},{'g','w','r'},{'r','y','y'}};
+    public static final char[][] down = {{'g','b','g'},{'g','w','r'},{'r','y','y'}};*/
+
+    public static final char[][] front = {{'r','o','b'},{'y','r','r'},{'g','b','o'}};
+    public static final char[][] back = {{'b','y','w'},{'y','o','w'},{'y','w','g'}};
+    public static final char[][] left = {{'y','o','r'},{'o','b','o'},{'o','b','w'}};
+    public static final char[][] right = {{'w','g','o'},{'g','g','w'},{'o','y','y'}};
+    public static final char[][] up = {{'g','r','r'},{'g','y','r'},{'b','w','w'}};
+    public static final char[][] down = {{'g','g','r'},{'b','w','b'},{'b','r','y'}};
 
 /*    private static char[][] r={{'y','b','r','o','y','g','o','y','b'},//up
             {'w','r','w','w','g','r','b','y','o'},//right
@@ -17,7 +24,7 @@ public class MagicCube {
             {'g','o','g','b','o','w','w','w','r'}};//back*/
     private static char[][] r={{up[0][0],up[0][1],up[0][2],up[1][0],up[1][1],up[1][2],up[2][0],up[2][1],up[2][2]},//up
             {right[2][0],right[1][0],right[0][0],right[2][1],right[1][1],right[0][1],right[2][2],right[1][2],right[0][2]},//right
-            {front[0][0],front[0][1],front[0][2],front[1][0],front[1][1],front[1][2],front[2][1],front[2][0],front[2][2]},//front
+            {front[0][0],front[0][1],front[0][2],front[1][0],front[1][1],front[1][2],front[2][0],front[2][1],front[2][2]},//front
             {down[2][2],down[2][1],down[2][0],down[1][2],down[1][1],down[1][0],down[0][2],down[0][1],down[0][0]},//down
             {left[0][2],left[1][2],left[2][2],left[0][1],left[1][1],left[2][1],left[0][0],left[1][0],left[2][0]},//left
             {back[2][2],back[2][1],back[2][0],back[1][2],back[1][1],back[1][0],back[0][2],back[0][1],back[0][0]}};//back
@@ -28,7 +35,11 @@ public class MagicCube {
         try
         {
             String resString=solveCube(r);
-            String[] res=resString.split(" ");
+            String[] res={};
+            if (resString!=null){
+                res=resString.split(" ");
+            }
+
             for (int i=0;i<res.length;i++){
                 String curString=res[res.length-1-i];
                 if (curString.contains("L")||curString.contains("B")||curString.contains("D")){
